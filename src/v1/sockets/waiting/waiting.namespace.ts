3f927b73d5438d77dbd7f14c24b992c40722cceb
kungbi <kipper12335@gmail.com>
2025-05-15 16:15:51 +0900

    feat(waiting): 불필요한 이벤트 핸들러 제거

--- src/v1/sockets/waiting/waiting.namespace.ts
+++ src/v1/sockets/waiting/waiting.namespace.ts
@@ -6,12 +6,4 @@ export function startWaitingNamespace(namespace: Namespace) {
   namespace.use(socketMiddleware);
 
   namespace.on('connection', (socket: Socket) => handleWaitingConnection(socket));
-
-  namespace.on('disconnect', (socket: Socket) => {
-    console.log(`🔴 [/waiting] Disconnected: ${socket.id}`);
-  });
-
-  namespace.on('error', (error: Error) => {
-    console.error(`Error in chat namespace: ${error.message}`);
-  });
 }
