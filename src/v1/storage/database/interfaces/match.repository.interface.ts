55f5362cc5a6716e7de6618d2d7f2105504949f9
kungbi <kipper12335@gmail.com>
2025-05-16 18:18:50 +0900

    feat(match): Prisma 클라이언트 라이브러리 변경

--- src/v1/storage/database/interfaces/match.repository.interface.ts
+++ src/v1/storage/database/interfaces/match.repository.interface.ts
@@ -1,5 +1,5 @@
 import { BaseRepositoryInterface } from './base.repository.interface.js';
-import { Match, Prisma } from '../../../../../generated/client/index.js';
+import { Match, Prisma } from '@prisma/client';
 
 export default interface MatchRepositoryInterface
   extends BaseRepositoryInterface<Match, Prisma.MatchCreateInput, Prisma.MatchUpdateInput> {}
