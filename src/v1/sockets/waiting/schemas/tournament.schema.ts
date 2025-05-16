40537a3c473d32b2444327c17e4dd01dd8d4f72c
kungbi <kipper12335@gmail.com>
2025-05-16 18:02:40 +0900

    feat(tournament): 토너먼트 크기에 16 추가

--- src/v1/sockets/waiting/schemas/tournament.schema.ts
+++ src/v1/sockets/waiting/schemas/tournament.schema.ts
@@ -2,7 +2,12 @@ import { TypeOf, z } from 'zod';
 
 export type tournamentSizeType = TypeOf<typeof tournamentSizeSchema>;
 
-export const tournamentSizeSchema = z.union([z.literal(2), z.literal(4), z.literal(8)]);
+export const tournamentSizeSchema = z.union([
+  z.literal(2),
+  z.literal(4),
+  z.literal(8),
+  z.literal(16),
+]);
 
 export type tournamentModeType = TypeOf<typeof tournamentModeSchema>;
 
