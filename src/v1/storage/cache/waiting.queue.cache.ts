92f71544c5b32716623d981f0f52ff8824f9a42b
kungbi <kipper12335@gmail.com>
2025-05-18 03:13:02 +0900

    fix(waiting.queue.cache): 오타수정 (writingQueueKey -> waitingQueueRedisKey)

--- src/v1/storage/cache/waiting.queue.cache.ts
+++ src/v1/storage/cache/waiting.queue.cache.ts
@@ -2,19 +2,19 @@ import { Redis } from 'ioredis';
 import { FastifyBaseLogger } from 'fastify';
 
 export default class WaitingQueueCache {
-  private readonly writingQueueKey;
+  private readonly waitingQueueRedisKey;
 
   constructor(
     private readonly redisClient: Redis,
     private readonly logger: FastifyBaseLogger,
     private readonly tournamentSize: number,
   ) {
-    this.writingQueueKey = `writing-queue:${this.tournamentSize}`;
+    this.waitingQueueRedisKey = `waiting-queue:${this.tournamentSize}`;
   }
 
   async addToQueue(userId: number): Promise<void> {
     this.logger.info(`Adding user ${userId} to waiting queue`);
-    await this.redisClient.rpush(this.writingQueueKey, userId);
+    await this.redisClient.rpush(this.waitingQueueRedisKey, userId);
   }
 
   getTournamentSize(): number {
@@ -27,7 +27,7 @@ export default class WaitingQueueCache {
   }
 
   async getQueueLength(): Promise<number> {
-    const queueLength = await this.redisClient.llen(this.writingQueueKey);
+    const queueLength = await this.redisClient.llen(this.waitingQueueRedisKey);
     this.logger.info(`Current queue length: ${queueLength}`);
     return queueLength;
   }
@@ -39,7 +39,7 @@ export default class WaitingQueueCache {
 
     const users: number[] = [];
     for (let i = 0; i < this.tournamentSize; i++) {
-      const userId = await this.redisClient.lpop(this.writingQueueKey);
+      const userId = await this.redisClient.lpop(this.waitingQueueRedisKey);
       if (!userId) throw new Error('No more users in the queue');
       users.push(Number(userId));
     }
