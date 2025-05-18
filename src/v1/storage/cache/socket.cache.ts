6f073d303b1eb8a01c0df3820c2a0419d78d4592
kungbi <kipper12335@gmail.com>
2025-05-19 01:22:33 +0900

    feat(socket.cache): 소켓 캐시 클래스 추가

---
+++ src/v1/storage/cache/socket.cache.ts
@@ -0,0 +1,24 @@
+import { Redis } from 'ioredis';
+
+export default class SocketCache {
+  constructor(private readonly redisClient: Redis) {}
+
+  private getKey(namespace: string, userId: number): string {
+    return `socket:${namespace}:${userId}`;
+  }
+
+  async setSocketId(input: { userId: number; socketId: string; namespace: string }): Promise<void> {
+    const key = this.getKey(input.namespace, input.userId);
+    await this.redisClient.set(key, input.socketId);
+  }
+
+  async getSocketId(input: { userId: number; namespace: string }): Promise<string | null> {
+    const key = this.getKey(input.namespace, input.userId);
+    return await this.redisClient.get(key);
+  }
+
+  async deleteSocketId(input: { userId: number; namespace: string }): Promise<void> {
+    const key = this.getKey(input.namespace, input.userId);
+    await this.redisClient.del(key);
+  }
+}
