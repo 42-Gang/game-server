9a11400a291768e0f2b0db9db5857ae57ddc1a53
kungbi <kipper12335@gmail.com>
2025-05-20 03:05:16 +0900

    feat(waiting): 소켓 이벤트 등록 함수 분리

--- src/v1/sockets/waiting/waiting.namespace.ts
+++ src/v1/sockets/waiting/waiting.namespace.ts
@@ -6,6 +6,30 @@ import WaitingSocketHandler from './waiting.socket.handler.js';
 import SocketCache from '../../storage/cache/socket.cache.js';
 import { SOCKET_EVENTS } from './waiting.event.js';
 
+function registerAutoEvents(socket: Socket, handler: WaitingSocketHandler, logger: any) {
+  socket.on(
+    SOCKET_EVENTS.AUTO.JOIN,
+    socketErrorHandler(socket, logger, (payload: autoJoinSchemaType) =>
+      handler.joinAutoRoom(socket, payload),
+    ),
+  );
+}
+
+function registerCustomEvents(socket: Socket, handler: WaitingSocketHandler, logger: any) {
+  socket.on(
+    SOCKET_EVENTS.CUSTOM.CREATE,
+    socketErrorHandler(socket, logger, (payload) => handler.createCustomRoom(socket, payload)),
+  );
+  socket.on(
+    SOCKET_EVENTS.CUSTOM.INVITE,
+    socketErrorHandler(socket, logger, (payload) => handler.inviteCustomRoom(socket, payload)),
+  );
+  socket.on(
+    SOCKET_EVENTS.CUSTOM.ACCEPT,
+    socketErrorHandler(socket, logger, (payload) => handler.acceptCustomRoom(socket, payload)),
+  );
+}
+
 export function startWaitingNamespace(namespace: Namespace) {
   namespace.use(socketMiddleware);
 
@@ -13,9 +37,9 @@ export function startWaitingNamespace(namespace: Namespace) {
     const waitingSocketHandler: WaitingSocketHandler =
       namespace.server.diContainer.resolve('waitingSocketHandler');
     const socketCache: SocketCache = namespace.server.diContainer.resolve('socketCache');
-
     const logger = namespace.server.logger;
     const userId = socket.data.userId;
+
     await socketCache.setSocketId({
       namespace: 'waiting',
       socketId: socket.id,
@@ -24,33 +48,8 @@ export function startWaitingNamespace(namespace: Namespace) {
 
     logger.info(`🟢 [/waiting] Connected: ${socket.id} ${userId}`);
 
-    socket.on(
-      SOCKET_EVENTS.AUTO.JOIN,
-      socketErrorHandler(socket, logger, (payload: autoJoinSchemaType) =>
-        waitingSocketHandler.joinAutoRoom(socket, payload),
-      ),
-    );
-
-    socket.on(
-      SOCKET_EVENTS.CUSTOM.CREATE,
-      socketErrorHandler(socket, logger, (payload) =>
-        waitingSocketHandler.createCustomRoom(socket, payload),
-      ),
-    );
-
-    socket.on(
-      SOCKET_EVENTS.CUSTOM.INVITE,
-      socketErrorHandler(socket, logger, (payload) =>
-        waitingSocketHandler.inviteCustomRoom(socket, payload),
-      ),
-    );
-
-    socket.on(
-      SOCKET_EVENTS.CUSTOM.ACCEPT,
-      socketErrorHandler(socket, logger, (payload) =>
-        waitingSocketHandler.acceptCustomRoom(socket, payload),
-      ),
-    );
+    registerAutoEvents(socket, waitingSocketHandler, logger);
+    registerCustomEvents(socket, waitingSocketHandler, logger);
 
     // TODO: 나가기 기능 추가
 
