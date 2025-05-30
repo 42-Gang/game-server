819b1dd69643114fba66920864ce999e3e7b0022
kungbi <kipper12335@gmail.com>
2025-05-31 02:10:06 +0900

    feat(tournament.player.cache.ts): 토너먼트 참가자 관련 정보 redis key 정의

--- src/v1/storage/cache/tournament/tournament.player.cache.ts
+++ src/v1/storage/cache/tournament/tournament.player.cache.ts
@@ -1,1 +1,23 @@
-export default class TournamentPlayerCache {}
+import { BASE_TOURNAMENT_KEY_PREFIX } from './tournament.cache.js';
+
+export default class TournamentPlayerCache {
+  private getPlayersKey(tournamentId: number): string {
+    return `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}:players`;
+  }
+
+  private getPlayerKey(tournamentId: number, userId: number): string {
+    return `${this.getPlayersKey(tournamentId)}:${userId}`;
+  }
+
+  private getPlayerStatusKey(tournamentId: number, userId: number): string {
+    return `${this.getPlayerKey(tournamentId, userId)}:status`;
+  }
+
+  private getActivePlayersKey(tournamentId: number): string {
+    return `${this.getPlayersKey(tournamentId)}:active`;
+  }
+
+  private getReadyPlayersKey(tournamentId: number): string {
+    return `${this.getPlayersKey(tournamentId)}:ready`;
+  }
+}
