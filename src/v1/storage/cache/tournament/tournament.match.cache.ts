dd2a75e129be18776c7718672899d2bab14703eb
kungbi <kipper12335@gmail.com>
2025-05-31 02:17:24 +0900

    feat(tournament.match.cache.ts): 매치 캐시 key 정의

--- src/v1/storage/cache/tournament/tournament.match.cache.ts
+++ src/v1/storage/cache/tournament/tournament.match.cache.ts
@@ -1,1 +1,15 @@
-export default class TournamentMatchCache {}
+import { BASE_TOURNAMENT_KEY_PREFIX } from './tournament.cache.js';
+
+export default class TournamentMatchCache {
+  private getMatchesKey(tournamentId: number): string {
+    return `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}:matches`;
+  }
+
+  private getMatchKey(tournamentId: number, matchId: number): string {
+    return `${this.getMatchesKey(tournamentId)}:${matchId}`;
+  }
+
+  private getMatchesByRoundKey(tournamentId: number, round: number): string {
+    return `${this.getMatchesKey(tournamentId)}:round:${round}`;
+  }
+}
