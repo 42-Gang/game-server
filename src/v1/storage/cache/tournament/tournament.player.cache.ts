5693a3a096c64df9fc1d47287e8e1af949f37e86
kungbi <kipper12335@gmail.com>
2025-05-31 02:34:52 +0900

    feat(tournament.player.cache.ts): player 등록 기능 추가

--- src/v1/storage/cache/tournament/tournament.player.cache.ts
+++ src/v1/storage/cache/tournament/tournament.player.cache.ts
@@ -1,6 +1,9 @@
 import { BASE_TOURNAMENT_KEY_PREFIX } from './tournament.cache.js';
+import { Redis } from 'ioredis';
 
 export default class TournamentPlayerCache {
+  constructor(private readonly redisClient: Redis) {}
+
   private getPlayersKey(tournamentId: number): string {
     return `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}:players`;
   }
@@ -9,7 +12,7 @@ export default class TournamentPlayerCache {
     return `${this.getPlayersKey(tournamentId)}:${userId}`;
   }
 
-  private getPlayerStatusKey(tournamentId: number, userId: number): string {
+  private getPlayerStateKey(tournamentId: number, userId: number): string {
     return `${this.getPlayerKey(tournamentId, userId)}:status`;
   }
 
@@ -19,5 +22,28 @@ export default class TournamentPlayerCache {
 
   private getReadyPlayersKey(tournamentId: number): string {
     return `${this.getPlayersKey(tournamentId)}:ready`;
+  }
+
+  private async initiatePlayersState(playerIds: number[], tournamentId: number) {
+    await Promise.all(
+      playerIds.map((playerId) =>
+        this.redisClient.set(this.getPlayerStateKey(tournamentId, playerId), 'NOT_READY'),
+      ),
+    );
+  }
+
+  private async addActivePlayers(tournamentId: number, playerIds: number[]) {
+    await this.redisClient.sadd(this.getActivePlayersKey(tournamentId), playerIds);
+  }
+
+  private async addPlayers(tournamentId: number, playerIds: number[]) {
+    const playersKey = this.getPlayersKey(tournamentId);
+    await this.redisClient.sadd(playersKey, playerIds);
+  }
+
+  async registerPlayers(tournamentId: number, playerIds: number[]): Promise<void> {
+    await this.addPlayers(tournamentId, playerIds);
+    await this.addActivePlayers(tournamentId, playerIds);
+    await this.initiatePlayersState(playerIds, tournamentId);
   }
 }
