ec7d12206326d93b3258ca2fa8acfc1deb5e87f2
kungbi <kipper12335@gmail.com>
2025-05-31 02:48:48 +0900

    feat(tournament.meta.cache.ts): 토너먼트 메타데이터 캐시 생성 기능 추가

--- src/v1/storage/cache/tournament/tournament.meta.cache.ts
+++ src/v1/storage/cache/tournament/tournament.meta.cache.ts
@@ -1,6 +1,18 @@
-import { BASE_TOURNAMENT_KEY_PREFIX } from './tournament.cache.js';
+import { BASE_TOURNAMENT_KEY_PREFIX, TOURNAMENT_TTL } from './tournament.cache.js';
+import { Redis } from 'ioredis';
+import { TypeOf, z } from 'zod';
+
+export const tournamentStateSchema = z.enum(['IN_PROGRESS', 'FINISHED']);
+
+export type tournamentMetaType = TypeOf<typeof tournamentMetaSchema>;
+export const tournamentMetaSchema = z.object({
+  mode: z.enum(['AUTO', 'CUSTOM']),
+  size: z.number(),
+});
 
 export default class TournamentMetaCache {
+  constructor(private readonly redisClient: Redis) {}
+
   private getTournamentKey(tournamentId: number): string {
     return `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}`;
   }
@@ -15,5 +27,32 @@ export default class TournamentMetaCache {
 
   private getTournamentCurrentRoundKey(tournamentId: number): string {
     return `${this.getTournamentKey(tournamentId)}:currentRound`;
+  }
+
+  private async initializeMeta(tournamentMetaKey: string, tournamentMeta: tournamentMetaType) {
+    await this.redisClient.hset(tournamentMetaKey, tournamentMeta);
+  }
+
+  private async setTournamentCurrentRound(tournamentCurrentRoundKey: string, size: number) {
+    await this.redisClient.set(tournamentCurrentRoundKey, size);
+  }
+
+  private async setTournamentState(tournamentStateKey: string) {
+    await this.redisClient.set(tournamentStateKey, tournamentStateSchema.enum.IN_PROGRESS);
+  }
+
+  async createTournamentMeta(tournamentId: number, metaData: tournamentMetaType): Promise<void> {
+    const tournamentMetaKey = this.getTournamentMetaKey(tournamentId);
+    const tournamentStateKey = this.getTournamentStateKey(tournamentId);
+    const tournamentCurrentRoundKey = this.getTournamentCurrentRoundKey(tournamentId);
+
+    await this.initializeMeta(tournamentMetaKey, metaData);
+    await this.setTournamentState(tournamentStateKey);
+    await this.setTournamentCurrentRound(tournamentCurrentRoundKey, metaData.size);
+
+    await this.redisClient.expire(
+      `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}:*`,
+      TOURNAMENT_TTL,
+    );
   }
 }
