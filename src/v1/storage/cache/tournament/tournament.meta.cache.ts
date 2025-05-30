18bcbb2712177fbce487ad8bd2fe233bfd4f711a
kungbi <kipper12335@gmail.com>
2025-05-31 02:12:46 +0900

    feat(tournament.meta.cache.ts): 토너먼트관련 정보(토너먼트 정보 및 상태 등) redis key 정의

--- src/v1/storage/cache/tournament/tournament.meta.cache.ts
+++ src/v1/storage/cache/tournament/tournament.meta.cache.ts
@@ -1,1 +1,19 @@
-export default class TournamentMetaCache {}
+import { BASE_TOURNAMENT_KEY_PREFIX } from './tournament.cache.js';
+
+export default class TournamentMetaCache {
+  private getTournamentKey(tournamentId: number): string {
+    return `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}`;
+  }
+
+  private getTournamentMetaKey(tournamentId: number): string {
+    return `${this.getTournamentKey(tournamentId)}:meta`;
+  }
+
+  private getTournamentStateKey(tournamentId: number): string {
+    return `${this.getTournamentKey(tournamentId)}:state`;
+  }
+
+  private getTournamentCurrentRoundKey(tournamentId: number): string {
+    return `${this.getTournamentKey(tournamentId)}:currentRound`;
+  }
+}
