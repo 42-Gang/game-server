efb730e35d1315c95c1cd50ccfbfcf7d8f3d57ab
kungbi <kipper12335@gmail.com>
2025-05-31 23:43:25 +0900

    feat(tournament.player.cache.ts): 토너먼트 플레이어 캐시 기능 추가

--- src/v1/storage/cache/tournament/tournament.player.cache.ts
+++ src/v1/storage/cache/tournament/tournament.player.cache.ts
@@ -1,8 +1,5 @@
 import { BASE_TOURNAMENT_KEY_PREFIX, TOURNAMENT_TTL } from './tournament.cache.js';
 import { Redis } from 'ioredis';
-import { z } from 'zod';
-
-const playerStateSchema = z.enum(['NOT_READY', 'READY', 'IN_GAME']);
 
 export default class TournamentPlayerCache {
   constructor(private readonly redisClient: Redis) {}
@@ -11,35 +8,16 @@ export default class TournamentPlayerCache {
     return `${BASE_TOURNAMENT_KEY_PREFIX}:${tournamentId}:players`;
   }
 
-  private getPlayerKey(tournamentId: number, userId: number): string {
-    return `${this.getPlayersKey(tournamentId)}:${userId}`;
-  }
-
-  private getPlayerStateKey(tournamentId: number, userId: number): string {
-    return `${this.getPlayerKey(tournamentId, userId)}:status`;
-  }
-
-  private getActivePlayersKey(tournamentId: number): string {
-    return `${this.getPlayersKey(tournamentId)}:active`;
-  }
-
   private getReadyPlayersKey(tournamentId: number): string {
     return `${this.getPlayersKey(tournamentId)}:ready`;
   }
 
-  private async initializePlayersState(playerIds: number[], tournamentId: number) {
-    await Promise.all(
-      playerIds.map((playerId) =>
-        this.redisClient.set(
-          this.getPlayerStateKey(tournamentId, playerId),
-          playerStateSchema.enum.NOT_READY,
-        ),
-      ),
-    );
+  private getPlayingPlayersKey(tournamentId: number): string {
+    return `${this.getPlayersKey(tournamentId)}:playing`;
   }
 
-  private async addActivePlayers(tournamentId: number, playerIds: number[]) {
-    await this.redisClient.sadd(this.getActivePlayersKey(tournamentId), playerIds);
+  private getEliminatedPlayersKey(tournamentId: number): string {
+    return `${this.getPlayersKey(tournamentId)}:eliminated`;
   }
 
   private async addPlayers(tournamentId: number, playerIds: number[]) {
@@ -47,11 +25,91 @@ export default class TournamentPlayerCache {
     await this.redisClient.sadd(playersKey, playerIds);
   }
 
+  private async setExpire(tournamentId: number) {
+    await this.redisClient.expire(`${this.getPlayersKey(tournamentId)}:*`, TOURNAMENT_TTL);
+  }
+
+  private async addPlayerInReady(tournamentId: number, userId: number) {
+    const readyPlayersKey = this.getReadyPlayersKey(tournamentId);
+    await this.redisClient.sadd(readyPlayersKey, userId);
+  }
+
+  private async isActivePlayer(tournamentId: number, userId: number): Promise<boolean> {
+    if (await this.redisClient.sismember(this.getEliminatedPlayersKey(tournamentId), userId)) {
+      return false;
+    }
+    return true;
+  }
+
   async registerPlayers(tournamentId: number, playerIds: number[]): Promise<void> {
     await this.addPlayers(tournamentId, playerIds);
-    await this.addActivePlayers(tournamentId, playerIds);
-    await this.initializePlayersState(playerIds, tournamentId);
 
+    await this.setExpire(tournamentId);
+  }
+
+  async setPlayerReady(tournamentId: number, userId: number): Promise<void> {
+    if (!(await this.isActivePlayer(tournamentId, userId))) {
+      throw new Error(`Player ${userId} is not an active player in tournament ${tournamentId}`);
+    }
+
+    await this.addPlayerInReady(tournamentId, userId);
+    await this.setExpire(tournamentId);
+  }
+
+  private async getReadyPlayers(tournamentId: number): Promise<number[]> {
+    const players = await this.redisClient.smembers(this.getReadyPlayersKey(tournamentId));
+    return players.map(Number);
+  }
+
+  private async getEliminatedPlayers(tournamentId: number): Promise<number[]> {
+    const players = await this.redisClient.smembers(this.getEliminatedPlayersKey(tournamentId));
+    return players.map(Number);
+  }
+
+  private async getPlayers(tournamentId: number): Promise<number[]> {
+    const players = await this.redisClient.smembers(this.getPlayersKey(tournamentId));
+    return players.map(Number);
+  }
+
+  async areAllPlayersReady(tournamentId: number): Promise<boolean> {
+    const readyPlayers = await this.getReadyPlayers(tournamentId);
+    const eliminatedPlayers = await this.getEliminatedPlayers(tournamentId);
+    const players = await this.getPlayers(tournamentId);
+
+    const readyCount = readyPlayers.length;
+    const activePlayersCount = players.length - eliminatedPlayers.length;
+
+    return readyCount === activePlayersCount;
+  }
+
+  async movePlayersToPlaying(tournamentId: number): Promise<void> {
+    const readyPlayersKey = this.getReadyPlayersKey(tournamentId);
+    const playingPlayersKey = this.getPlayingPlayersKey(tournamentId);
+
+    const readyPlayers = await this.getReadyPlayers(tournamentId);
+    await this.redisClient
+      .multi()
+      .srem(readyPlayersKey, readyPlayers)
+      .sadd(playingPlayersKey, readyPlayers)
+      .exec();
+
+    await this.refreshTTL(tournamentId);
+  }
+
+  async eliminatePlayer(tournamentId: number, userId: number): Promise<void> {
+    const playersKey = this.getPlayersKey(tournamentId);
+    const eliminatedPlayersKey = this.getEliminatedPlayersKey(tournamentId);
+
+    await this.redisClient
+      .multi()
+      .srem(playersKey, userId)
+      .sadd(eliminatedPlayersKey, userId)
+      .exec();
+
+    await this.refreshTTL(tournamentId);
+  }
+
+  private async refreshTTL(tournamentId: number) {
     await this.redisClient.expire(`${this.getPlayersKey(tournamentId)}:*`, TOURNAMENT_TTL);
   }
 }
