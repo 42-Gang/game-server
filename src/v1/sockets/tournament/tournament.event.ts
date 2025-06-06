13f856d3d3f8f92c9e6f3d55ab08e8c9d01fbf00
kungbi <kipper12335@gmail.com>
2025-06-07 00:43:09 +0900

    feat(tournament.event.ts): 토너먼트 타입 정의 추가

--- src/v1/sockets/tournament/tournament.event.ts
+++ src/v1/sockets/tournament/tournament.event.ts
@@ -6,6 +6,23 @@ export const TOURNAMENT_SOCKET_EVENTS = {
   GAME_RESULT: 'game-result',
 };
 
+export const playerCacheSocketSchema = z.object({
+  userId: z.number(),
+  nickname: z.string(),
+  profileImage: z.string(),
+  isReady: z.boolean(),
+});
+
+export type PlayerCacheSocketType = z.infer<typeof playerCacheSocketSchema>;
+
+export const tournamentInfoSchema = z.object({
+  mode: z.enum(['AUTO', 'CUSTOM']),
+  size: z.number(),
+  players: z.array(playerCacheSocketSchema),
+});
+
+export type TournamentInfoType = z.infer<typeof tournamentInfoSchema>;
+
 export const broadcastAllUsersReadySchema = z.object({
   type: z.literal('all-users-ready'),
 });
