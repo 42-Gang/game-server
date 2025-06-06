8db68adcffdd7028f008a159175c3c85e8e51ccf
kungbi <kipper12335@gmail.com>
2025-06-07 00:43:03 +0900

    feat(tournament.cache.ts): 토너먼트 생성 시 플레이어 정보 캐싱

--- src/v1/storage/cache/tournament/tournament.cache.ts
+++ src/v1/storage/cache/tournament/tournament.cache.ts
@@ -4,6 +4,8 @@ import TournamentMatchCache, { matchSchema } from './tournament.match.cache.js';
 import TournamentMetaCache from './tournament.meta.cache.js';
 import TournamentPlayerCache from './tournament.player.cache.js';
 import { TypeOf, z } from 'zod';
+import PlayerCache from '../player.cache.js';
+import UserServiceClient from '../../../client/user.service.client.js';
 
 export const BASE_TOURNAMENT_KEY_PREFIX = 'tournament';
 export const TOURNAMENT_TTL = 60 * 30;
@@ -24,6 +26,8 @@ export default class TournamentCache {
     private readonly tournamentMatchCache: TournamentMatchCache,
     private readonly tournamentMetaCache: TournamentMetaCache,
     private readonly tournamentPlayerCache: TournamentPlayerCache,
+    private readonly playerCache: PlayerCache,
+    private readonly userServiceClient: UserServiceClient,
   ) {}
 
   async createTournament(input: createTournamentType): Promise<void> {
@@ -37,10 +41,30 @@ export default class TournamentCache {
       this.tournamentMatchCache.createMatch(input.tournamentId, match.id, match),
     );
     await this.tournamentPlayerCache.registerPlayers(input.tournamentId, input.playerIds);
+    await this.fetchPlayersInfo(input.playerIds);
+  }
+
+  private async fetchPlayersInfo(playerIds: number[]) {
+    for (const playerId of playerIds) {
+      if (await this.playerCache.isExists(playerId)) continue;
+
+      this.logger.info(`🟢 [/tournament] Fetching user info for ${playerId}`);
+      const user = await this.userServiceClient.getUserInfo(playerId);
+      await this.playerCache.setPlayer(playerId, {
+        id: user.id,
+        nickname: user.nickname,
+        avatar: user.avatarUrl,
+      });
+    }
   }
 
   getTournamentInfo(tournamentId: number) {
     this.logger.info(`Retrieving tournament meta for ${tournamentId}`);
     return this.tournamentMetaCache.getTournamentInfo(tournamentId);
+  }
+
+  getAllPlayerIds(tournamentId: number) {
+    this.logger.info(`Retrieving all player IDs for tournament ${tournamentId}`);
+    return this.tournamentPlayerCache.getAllPlayerIds(tournamentId);
   }
 }
