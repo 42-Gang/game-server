c7a9a6f546b9f04834b767a5f2bf266a446db814
kungbi <kipper12335@gmail.com>
2025-06-08 01:47:36 +0900

    feat(match.topic.schema.ts): 스키마에 토너먼트 ID 추가

--- src/v1/kafka/producers/match.producer.ts
+++ src/v1/kafka/producers/match.producer.ts
@@ -3,11 +3,13 @@ import { TOPICS } from '../constants.js';
 import { matchRequestMessageSchema } from '../schemas/match.topic.schema.js';
 
 export async function matchRequestProducer(input: {
+  tournameId: string;
   matchId: number;
   player1Id: number;
   player2Id: number;
 }) {
   const message = matchRequestMessageSchema.parse({
+    tournamentId: input.tournameId,
     matchId: input.matchId,
     player1Id: input.player1Id,
     player2Id: input.player2Id,
--- src/v1/kafka/schemas/match.topic.schema.ts
+++ src/v1/kafka/schemas/match.topic.schema.ts
@@ -3,6 +3,7 @@ import { MATCH_EVENTS } from '../constants.js';
 
 export const matchRequestMessageSchema = z.object({
   eventType: z.literal(MATCH_EVENTS.REQUEST),
+  tournamentId: z.number(),
   matchId: z.number(),
   player1Id: z.number(),
   player2Id: z.number(),
