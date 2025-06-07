c5c8bdc1f9af0aed78ca61926cd5631f539d656f
kungbi <kipper12335@gmail.com>
2025-06-08 00:48:51 +0900

    refactor(match.producer.ts): 매치 요청 프로듀서 인자 변경

--- src/v1/kafka/producers/match.producer.ts
+++ src/v1/kafka/producers/match.producer.ts
@@ -2,11 +2,15 @@ import { producer } from '../../../plugins/kafka.js';
 import { TOPICS } from '../constants.js';
 import { matchRequestMessageSchema } from '../schemas/match.topic.schema.js';
 
-export async function matchRequestProducer(matchId: number, player1Id: number, player2Id: number) {
+export async function matchRequestProducer(input: {
+  matchId: number;
+  player1Id: number;
+  player2Id: number;
+}) {
   const message = matchRequestMessageSchema.parse({
-    matchId,
-    player1Id,
-    player2Id,
+    matchId: input.matchId,
+    player1Id: input.player1Id,
+    player2Id: input.player2Id,
     timestamp: new Date().toISOString(),
   });
 
