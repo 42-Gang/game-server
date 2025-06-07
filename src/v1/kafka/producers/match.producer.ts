521486638b79cbb210c715513aac54fad96f3f1a
kungbi <kipper12335@gmail.com>
2025-06-07 16:38:38 +0900

    feat(match.producer.ts): 매치 생성 프로듀서 구현

--- src/v1/kafka/producers/match.producer.ts
+++ src/v1/kafka/producers/match.producer.ts
@@ -1,1 +1,16 @@
-// TODO: 게임시작을 위한 매치 생성 프로듀서
+import { producer } from '../../../plugins/kafka.js';
+import { TOPICS } from '../constants.js';
+import { matchRequestMessageSchema } from '../schemas/match.topic.schema.js';
+
+export async function matchRequestProducer(matchId: number, player1Id: number, player2Id: number) {
+  const message = matchRequestMessageSchema.parse({
+    matchId,
+    player1Id,
+    player2Id,
+  });
+
+  await producer.send({
+    topic: TOPICS.MATCH,
+    messages: [message],
+  });
+}
