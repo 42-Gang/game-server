b0f670292f235b0b75699158a60177c02eb16afa
kungbi <kipper12335@gmail.com>
2025-06-08 00:41:11 +0900

    comment(match.topic.service.ts): TODO 작성

--- src/v1/kafka/consumers/match.topic.service.ts
+++ src/v1/kafka/consumers/match.topic.service.ts
@@ -9,9 +9,15 @@ type HandleMatchResultInput = TypeOf<typeof handleMatchResultInputSchema>;
 export default class MatchTopicService {
   async handleMatchCreated(messageValue: HandleMatchCreatedInput): Promise<void> {
     handleMatchCreatedInputSchema.parse(messageValue);
+
+    // TODO: 매치정보(서버정보 및 매치 ID등) 해당 매치에 참여한 유저들에게 전달
   }
 
   async handleMatchResult(messageValue: HandleMatchResultInput): Promise<void> {
     handleMatchResultInputSchema.parse(messageValue);
+
+    // TODO: 매치 결과 DB에 저장
+
+    // TODO: 매치 결과(승패 및 점수 등) 해당 토너먼트에 참여한 유저들에게 전달
   }
 }
