2a23c72afc1ad4324a6eb76fc7438d5785f28d7f
kungbi <kipper12335@gmail.com>
2025-06-08 02:03:14 +0900

    feat(match.producer.ts): 매치생성 요청에 서버 이름 추가

--- src/v1/kafka/producers/match.producer.ts
+++ src/v1/kafka/producers/match.producer.ts
@@ -5,12 +5,14 @@ import { matchRequestMessageSchema } from '../schemas/match.topic.schema.js';
 export async function matchRequestProducer(input: {
   tournamentId: string;
   matchId: number;
+  matchServerName: string;
   player1Id: number;
   player2Id: number;
 }) {
   const message = matchRequestMessageSchema.parse({
     tournamentId: input.tournamentId,
     matchId: input.matchId,
+    matchServerName: input.matchServerName,
     player1Id: input.player1Id,
     player2Id: input.player2Id,
     timestamp: new Date().toISOString(),
