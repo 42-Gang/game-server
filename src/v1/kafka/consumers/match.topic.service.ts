8cbf8e1dd62c7b4484a6d20a8ef2de7e6744f679
kungbi <kipper12335@gmail.com>
2025-06-08 15:58:52 +0900

    feat(match.topic.service.ts): 매치 결과 처리 로직 추가

--- src/v1/kafka/consumers/match.topic.service.ts
+++ src/v1/kafka/consumers/match.topic.service.ts
@@ -2,13 +2,14 @@ import { TypeOf, z } from 'zod';
 import { Namespace } from 'socket.io';
 import SocketCache from '../../storage/cache/socket.cache.js';
 import { TOURNAMENT_SOCKET_EVENTS } from '../../sockets/tournament/tournament.event.js';
+import TournamentPlayerCache from '../../storage/cache/tournament/tournament.player.cache.js';
+import TournamentMatchCache from '../../storage/cache/tournament/tournament.match.cache.js';
+import TournamentMetaCache from '../../storage/cache/tournament/tournament.meta.cache.js';
 
 const handleMatchCreatedInputSchema = z.object({
   tournamentId: z.number(),
   matchId: z.number(),
-  serverInfo: z.object({
-    serverName: z.string(),
-  }),
+  serverName: z.string(),
   player1Id: z.number(),
   player2Id: z.number(),
 });
@@ -24,6 +25,7 @@ const handleMatchResultInputSchema = z.object({
     player2: z.number(),
   }),
   winnerId: z.number(),
+  loserId: z.number(),
   round: z.number(),
 });
 type HandleMatchResultInputType = TypeOf<typeof handleMatchResultInputSchema>;
@@ -32,6 +34,9 @@ export default class MatchTopicService {
   constructor(
     private readonly tournamentNamespace: Namespace,
     private readonly socketCache: SocketCache,
+    private readonly tournamentPlayerCache: TournamentPlayerCache,
+    private readonly tournamentMatchCache: TournamentMatchCache,
+    private readonly tournamentMetaCache: TournamentMetaCache,
   ) {}
 
   async handleMatchCreated(messageValue: HandleMatchCreatedInputType): Promise<void> {
@@ -46,11 +51,34 @@ export default class MatchTopicService {
   async handleMatchResult(messageValue: HandleMatchResultInputType): Promise<void> {
     handleMatchResultInputSchema.parse(messageValue);
 
-    // TODO: 매치 결과 DB에 저장
+    // TODO: 매치 결과 DB에 저장 및 다음 라운드 반영
+
+    await this.tournamentPlayerCache.movePlayerToEliminated(
+      messageValue.tournamentId,
+      messageValue.loserId,
+    );
+
+    await this.tournamentMatchCache.removeMatchInRound(
+      messageValue.tournamentId,
+      messageValue.round,
+      messageValue.matchId,
+    );
+
+    if (
+      await this.tournamentMatchCache.isEmptyInRound(messageValue.tournamentId, messageValue.round)
+    ) {
+      await this.tournamentMetaCache.moveToNextRound(messageValue.tournamentId);
+    }
 
     this.tournamentNamespace
       .to(`tournament:${messageValue.tournamentId}`)
       .emit(TOURNAMENT_SOCKET_EVENTS.GAME_RESULT, messageValue);
+
+    if (await this.tournamentMetaCache.isFinished(messageValue.tournamentId)) {
+      this.tournamentNamespace
+        .to(`tournament:${messageValue.tournamentId}`)
+        .emit(TOURNAMENT_SOCKET_EVENTS.FINISHED);
+    }
   }
 
   private async getSocketIds(playerIds: number[]) {
