44aec5d5226d0899c733fc4543bcc5395eb0b14e
kungbi <kipper12335@gmail.com>
2025-07-08 22:14:53 +0900

    refactor(tournament.namespace.ts): 예외 처리 추가

--- src/v1/sockets/tournament/tournament.namespace.ts
+++ src/v1/sockets/tournament/tournament.namespace.ts
@@ -4,6 +4,7 @@ import SocketCache from '../../storage/cache/socket.cache.js';
 import { tournamentMiddleware } from './tournament.middleware.js';
 import { TOURNAMENT_SOCKET_EVENTS } from './tournament.event.js';
 import TournamentSocketHandler from './handlers/tournament.socket.handler.js';
+import { socketErrorHandler } from '../utils/errorHandler.js';
 
 export function startTournamentNamespace(namespace: Namespace) {
   namespace.use(socketMiddleware);
@@ -20,21 +21,33 @@ export function startTournamentNamespace(namespace: Namespace) {
     const userId = socket.data.userId;
     const tournamentId = socket.data.tournamentId;
 
-    await socketCache.setSocketId({
-      namespace: 'tournament',
-      socketId: socket.id,
-      userId: userId,
-    });
-    await tournamentSocketHandler.sendBracket(socket);
-    socket.join(`tournament:${tournamentId}`);
+    try {
+      await socketCache.setSocketId({
+        namespace: 'tournament',
+        socketId: socket.id,
+        userId: userId,
+      });
+      await tournamentSocketHandler.sendBracket(socket);
+      socket.join(`tournament:${tournamentId}`);
+      await tournamentSocketHandler.sendTournamentInfo(socket);
 
-    logger.info(`🟢 [/tournament] Connected: ${socket.id} ${userId}`);
-    await tournamentSocketHandler.sendTournamentInfo(socket);
+      logger.info(`🟢 [/tournament] Connected: ${socket.id} ${userId}`);
+    } catch (error) {
+      logger.error(
+        `Error during connection for socket ${socket.id} and user ${userId}: ${
+          error instanceof Error ? error.message : error
+        }`,
+      );
+      return;
+    }
 
-    socket.on(TOURNAMENT_SOCKET_EVENTS.READY, async () => {
-      logger.info(`🟢 [/tournament] User ${userId} is ready`);
-      await tournamentSocketHandler.handleReady(socket);
-    });
+    socket.on(
+      TOURNAMENT_SOCKET_EVENTS.READY,
+      socketErrorHandler(socket, logger, async () => {
+        logger.info(`🟢 [/tournament] User ${userId} is ready`);
+        await tournamentSocketHandler.handleReady(socket);
+      }),
+    );
 
     socket.on('disconnect', () => {
       logger.info(`🔴 [/waiting] Disconnected: ${socket.id}`);
